64df74cdf68bcc4b1a081c8e5bd6ed17615d6686
Sergio Otzoy <s.otzoy.97@outlook.com>
2022-10-08 12:28:59 -0600

    se removió flask

--- unidadSiete/sesionOnce/ejemplo12/app.py
+++ unidadSiete/sesionOnce/ejemplo12/app.py
@@ -1,11 +1,9 @@
 from flask import Flask
-from flask_cors import CORS
 from client.routes.client_routes import client
 from account.routes.account_routes import account
 from transfer.routes.transfer_routes import transfer
 
 app = Flask(__name__)
-CORS(app)
 
 @app.route('/')
 def index():
